19cb718973cef14c7975eb14847ee84ac398300c
schu38 <sandy.chu.40@berkeley.edu>
2018-02-04 22:24:44 -0800

    Create cone-of-lines.js

---
+++ cone-of-lines.js
@@ -0,0 +1,12 @@
+
+//can add more lines to the cone by making the i < 300 condition larger
+stroke(255);
+background(0);
+strokeWeight(1);
+
+for (i = 30; i < 300; i = i + 30) {
+
+  delX = 25 + i;
+  
+  line(175, 20, delX, 300)
+}
